77e740f4639a43c35bfe1c4306e7c21b48e12909
V9 Developer <99959044+v9dev@users.noreply.github.com>
2025-01-26 17:44:36 +0530

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key:"YOUR API KEY",
+        key:"2e9ade8ec33f48f8abf3a3418b920de0",
     }
 })
